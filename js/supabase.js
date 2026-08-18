88cb77be01a93cf9dc5548e7f1518d6a0c95b520
henriqux77 <duartehenrique245@gmail.com>
2026-08-18 16:16:21 -0300

    Atualizar o supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,23 @@
 const SUPABASE_URL = "https://xrofkyinattalxmirwdq.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_CM4mEo6GOLZvsKTKLiMU9A_N1bOjqAH";
 
-window.supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_CM4mEo6GOLZvsKTKLiMU9A_N1bOjqAH";
+
+
+window.supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY,
+        {
+            auth: {
+                persistSession: true,
+                autoRefreshToken: true,
+                detectSessionInUrl: true
+            }
+        }
+    );
+
+
+console.log(
+    "Aerion conectado ao Supabase!"
 );
